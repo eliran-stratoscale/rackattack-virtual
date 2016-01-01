3e30b38b95fea79eb86b2f442f63710fe464594a
Eliran Levi <vandelayind@gmail.com>
2016-01-01 16:11:08 +0200

    Don't start rabbitmq-server when intalling on Ubuntu

--- validate_packages_prerequisites.sh
+++ validate_packages_prerequisites.sh
@@ -10,6 +10,8 @@ rpm --import https://www.rabbitmq.com/rabbitmq-signing-key-public.asc;
 sudo yum install -y --nogpg rabbitmq-server;
 elif [[ "$APT_GET_CMD" != "" ]]; then
 sudo apt-get -y install libvirt-bin libvirt-dev syslinux pxelinux rabbitmq-server;
+sudo systemctl stop rabbimq-server
+sudo systemctl disable rabbimq-server
 else
 echo "Error: Package manager was not found. Cannot continue with the installation.";
 exit 1;
