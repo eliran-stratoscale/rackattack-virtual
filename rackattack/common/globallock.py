4a4cb336839d42cee872e52399e17249b948492a
Eliran Levi <eliran@stratoscale.com>
2016-03-13 14:47:21 +0200

    Print the stack info more clearly, when holding the global lock for too long

--- rackattack/common/globallock.py
+++ rackattack/common/globallock.py
@@ -8,6 +8,10 @@ import logging
 _lock = threading.Lock()
 
 
+def prettyStack():
+    return "\n".join([line.strip() for line in traceback.format_stack()])
+
+
 @contextlib.contextmanager
 def lock():
     before = time.time()
@@ -17,14 +21,14 @@ def lock():
         if took > 0.1:
             logging.error(
                 "Acquiring the global lock took more than 0.1s: %(took)ss. Stack:\n%(stack)s", dict(
-                    took=took, stack=traceback.format_stack()))
+                    took=took, stack=prettyStack()))
         yield
         released = time.time()
         took = released - acquired
         if took > 0.3:
             logging.error(
                 "Holding the global lock took more than 0.1s: %(took)ss. Stack:\n%(stack)s", dict(
-                    took=took, stack=traceback.format_stack()))
+                    took=took, stack=prettyStack()))
 
 
 def assertLocked():
