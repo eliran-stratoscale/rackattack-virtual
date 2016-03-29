1400d02f2ad1f82c6e730d31069d92224da3e959
Eliran Levi <eliran@stratoscale.com>
2016-03-29 17:19:51 +0300

    Fix typo in prerequisites script

--- sh/validate_packages_prerequisites.sh
+++ sh/validate_packages_prerequisites.sh
@@ -10,8 +10,8 @@ rpm --import https://www.rabbitmq.com/rabbitmq-signing-key-public.asc;
 sudo yum install -y --nogpg rabbitmq-server;
 elif [[ "$APT_GET_CMD" != "" ]]; then
 sudo apt-get -y install libvirt-bin libvirt-dev syslinux pxelinux rabbitmq-server qemu-kvm net-tools;
-sudo systemctl stop rabbimq-server
-sudo systemctl disable rabbimq-server
+sudo systemctl stop rabbitmq-server
+sudo systemctl disable rabbitmq-server
 else
 echo "Error: Package manager was not found. Cannot continue with the installation.";
 exit 1;
